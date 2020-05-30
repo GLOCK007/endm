5ad927aa70ccdfafbd3786a6b1bf2f8df83bb839
GLOCK007 <65655182+GLOCK007@users.noreply.github.com>
2020-05-30 14:41:41 +0530

    Update universe.py

--- universe.py
+++ universe.py
@@ -99,5 +99,5 @@ async def on_ready():
     print('Created by Tejas')
 
     
-client.run("NzE2MjE0MDU0NDE4MzgyODcw.XtIigA.FNbs4XpW2KU8wIDUS8VvttuASI8")                
+client.run("NzE2MjE0MDU0NDE4MzgyODcw.XtIi2g.Sr9oWKoq6v6lEo2UObnP-bs8K2s")                
 
